9ef3abf092d16c741946be28a841ef72b01aa1fe
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-06-20 20:49:04 +0530

    corrected variable names

--- backend/visualizer.py
+++ backend/visualizer.py
@@ -18,20 +18,20 @@ def supervised_list():
 
 @viz.route('/home/unsupervised/list', methods=['GET'])
 def unsupervised_list():
-    sup_list = os.listdir('Unsupervised_Learning/')
-    return jsonify([x for x in sup_list if x != '__init__.py'])
+    unsup_list = os.listdir('Unsupervised_Learning/')
+    return jsonify([x for x in unsup_list if x != '__init__.py'])
 
 
 @viz.route('/home/reinforcement/list', methods=['GET'])
 def reinforcement_list():
-    sup_list = os.listdir('Reinforcement_Learning/')
-    return jsonify([x for x in sup_list if x != '__init__.py'])
+    reinf_list = os.listdir('Reinforcement_Learning/')
+    return jsonify([x for x in reinf_list if x != '__init__.py'])
 
 
 @viz.route('/home/neural/list', methods=['GET'])
 def neural_list():
-    sup_list = os.listdir('Neural_Networks/')
-    return jsonify([x for x in sup_list if x != '__init__.py'])
+    neur_list = os.listdir('Neural_Networks/')
+    return jsonify([x for x in neur_list if x != '__init__.py'])
 
 
 if __name__ == '__main__':
