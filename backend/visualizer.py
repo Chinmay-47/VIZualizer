50bd4f67a469f6d1dab8bd63b3e89b750712d8a8
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-06-20 16:34:53 +0530

    kept the underscore in returned directory names in home page

--- backend/visualizer.py
+++ backend/visualizer.py
@@ -13,25 +13,25 @@ def index():
 @viz.route('/home/supervised/list', methods=['GET'])
 def supervised_list():
     sup_list = os.listdir('Supervised_Learning/')
-    return jsonify([x.replace('_', ' ') for x in sup_list if x != '__init__.py'])
+    return jsonify([x for x in sup_list if x != '__init__.py'])
 
 
 @viz.route('/home/unsupervised/list', methods=['GET'])
 def unsupervised_list():
     sup_list = os.listdir('Unsupervised_Learning/')
-    return jsonify([x.replace('_', ' ') for x in sup_list if x != '__init__.py'])
+    return jsonify([x for x in sup_list if x != '__init__.py'])
 
 
 @viz.route('/home/reinforcement/list', methods=['GET'])
 def reinforcement_list():
     sup_list = os.listdir('Reinforcement_Learning/')
-    return jsonify([x.replace('_', ' ') for x in sup_list if x != '__init__.py'])
+    return jsonify([x for x in sup_list if x != '__init__.py'])
 
 
 @viz.route('/home/neural/list', methods=['GET'])
 def neural_list():
     sup_list = os.listdir('Neural_Networks/')
-    return jsonify([x.replace('_', ' ') for x in sup_list if x != '__init__.py'])
+    return jsonify([x for x in sup_list if x != '__init__.py'])
 
 
 if __name__ == '__main__':
