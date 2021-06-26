6e38eb5419d967b27561c1baabb043ab9b551c53
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-06-27 01:15:05 +0530

    added linear regression api to check

--- backend/visualizer.py
+++ backend/visualizer.py
@@ -1,7 +1,10 @@
 import os
-
+import io
 from flask import Flask, jsonify, Response
 from flask_cors import CORS, cross_origin
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+from .Supervised_Learning.Linear_Regression.Linear_Regression import LinearRegressionVisualizer
 
 viz = Flask(__name__)
 CORS(viz, support_credentials=True)
@@ -19,6 +22,24 @@ def get_topics_list() -> Response:
                    and sub_topic.name != '__pycache__'] for topic in topics]
 
     return jsonify(list(zip(topics, sub_topics)))
+
+
+@viz.route('/home/linear-regression', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def get_linear_Regression_plots() -> Response:
+
+    vizualizer = LinearRegressionVisualizer()
+    fig1 = vizualizer.show_data(return_fig=True)
+    # fig2 = vizualizer.show_initial_regression_line(return_fig=True)
+    # vizualizer.train()
+    # fig3 = vizualizer.show_regression_line_comparison(return_fig=True)
+    output1 = io.BytesIO()
+    # output2 = io.BytesIO()
+    # output3 = io.BytesIO()
+    FigureCanvas(fig1).print_png(output1)
+    # FigureCanvas(fig2).print_png(output2)
+    # FigureCanvas(fig3).print_png(output3)
+    return Response(output1.getvalue(), mimetype='image/png')
 
 
 if __name__ == '__main__':
